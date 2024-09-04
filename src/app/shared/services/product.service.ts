5d4bda0d30ee2a42cc9fdea96b1136008574ccbf
Salfi Sabit <shalfi.sabit@brainstation23.com>
2024-09-04 14:47:36 +0600

    create product service

---
+++ src/app/shared/services/product.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../models/Product';
+
+@Injectable({ providedIn: 'root' })
+export class ProductService {
+  private localStorageKey = 'products';
+  products: Product[] = [];
+
+  constructor() {
+    const savedProducts = localStorage.getItem(this.localStorageKey);
+    this.products = savedProducts ? JSON.parse(savedProducts) : [];
+  }
+
+  getProducts(): Product[] {
+    return this.products;
+  }
+
+  addProduct(product: Product): void {
+    this.products.push(product);
+    this.updateLocalStorage();
+  }
+
+  editProduct(productId: string, updatedProduct: Product) {
+    const index = this.products.findIndex(
+      (product) => product.id === productId
+    );
+
+    if (index != -1) {
+      this.products[index] = { ...updatedProduct, id: productId };
+      this.updateLocalStorage();
+    }
+  }
+
+  deleteProduct(productId: string): void {
+    this.products = this.products.filter((product) => product.id !== productId);
+    this.updateLocalStorage();
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.products));
+  }
+}
