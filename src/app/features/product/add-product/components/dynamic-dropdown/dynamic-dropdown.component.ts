714817b37a356f258a5cce1abaf7f9e9ef3eb199
salf1-sabit <salfisabit@gmail.com>
2024-09-05 23:10:31 +0600

    add dynamic dropdown

--- src/app/features/product/add-product/components/dynamic-dropdown/dynamic-dropdown.component.ts
+++ src/app/features/product/add-product/components/dynamic-dropdown/dynamic-dropdown.component.ts
@@ -1,5 +1,17 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  forwardRef,
+  OnInit,
+} from '@angular/core';
+import {
+  FormControl,
+  ReactiveFormsModule,
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 import { NgFor, NgIf } from '@angular/common';
 
 @Component({
@@ -7,9 +19,16 @@ import { NgFor, NgIf } from '@angular/common';
   selector: 'app-dynamic-dropdown',
   templateUrl: './dynamic-dropdown.component.html',
   styleUrls: ['./dynamic-dropdown.component.css'],
-  imports: [NgIf, NgFor],
+  imports: [NgIf, NgFor, ReactiveFormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DynamicDropdownComponent),
+      multi: true,
+    },
+  ],
 })
-export class DynamicDropdownComponent {
+export class DynamicDropdownComponent implements ControlValueAccessor, OnInit {
   @Input() formControl!: FormControl;
   @Input() existingCategories: string[] = [];
   @Output() newCategoryAdded = new EventEmitter<string>();
@@ -17,13 +36,68 @@ export class DynamicDropdownComponent {
   newCategoryControl = new FormControl('');
   showAddInput: boolean = false;
 
+  value: string | null = null;
+  onChange: (value: string | null) => void = () => {};
+  onTouched: () => void = () => {};
+
+  ngOnInit() {
+    this.loadCategoriesFromLocalStorage();
+  }
+
+  writeValue(value: string | null): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: (value: string | null) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    if (isDisabled) {
+      this.newCategoryControl.disable();
+    } else {
+      this.newCategoryControl.enable();
+    }
+  }
+
   addNewCategory() {
     const newCategory = this.newCategoryControl.value;
-    if (newCategory && !this.existingCategories.includes(newCategory)) {
-      this.existingCategories.push(newCategory);
+    if (newCategory) {
       this.newCategoryAdded.emit(newCategory);
-      this.newCategoryControl.setValue(''); // Clear input
+      this.value = newCategory;
+      this.onChange(this.value);
+      this.loadCategoriesFromLocalStorage();
+      this.newCategoryControl.setValue('');
       this.showAddInput = false;
+    }
+  }
+
+  onSelectChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.value = selectElement.value;
+    this.onChange(this.value);
+    this.onTouched();
+  }
+
+  loadCategoriesFromLocalStorage() {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const storedCategories = localStorage.getItem('categories');
+      if (storedCategories) {
+        this.existingCategories = JSON.parse(storedCategories);
+      }
+    }
+  }
+
+  updateCategoriesInLocalStorage() {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      localStorage.setItem(
+        'categories',
+        JSON.stringify(this.existingCategories)
+      );
     }
   }
 }
